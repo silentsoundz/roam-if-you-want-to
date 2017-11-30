db11adfaf516a849e951a8a0d51e6ef78df1c1fa
Jenn Wilde <jennowilde@gmail.com>
2017-11-30 14:55:41 -0800

    Signup page and auth/signup post route

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -2,6 +2,8 @@ const members = require('../../models/database/members')
 const router = require('express').Router()
 const bcrypt = require('bcrypt')
 
+router.get('/', (request, response) => {
+  response.render('auth/signup')
+})
 
-
-
+module.exports = router;
