1b5b283ab59e43e7bd19595b7dcd1ae6b5678390
Stephani McGrath <silentsoundz1977@gmail.com>
2017-12-11 14:15:24 -0800

    makes seed data for post

--- src/models/database/schema/seed-data.sql
+++ src/models/database/schema/seed-data.sql
@@ -4,3 +4,13 @@ INSERT INTO
   ('Frodo Baggins', 'frobag', 'fbaggins@shire.com', 'http://www.thelandofshadow.com/wp-content/uploads/2015/04/frodo-goodbyeb.jpg', 'theonering', 'Bozeman'),
   ('Samwise Gamgee', 'turnipG', 'hoesinspades@shire.com', 'http://image.guardian.co.uk/sys-images/Film/Pix/gallery/2002/10/29/samwise3.jpg', 'turnips', 'Chicago'),
   ('Galadriel', 'Artanis', 'elfqueen@lotholori.en', 'https://qph.ec.quoracdn.net/main-qimg-31e131c8c13a56c4ee82ae6fad069200', 'queenE', 'New Orleans')
+
+INSERT INTO
+post (title, member_id, post_entry)
+VALUES
+('The Hobbits', 1, "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc., li tot Europa usa li sam vocabularium. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules. Omnicos directe al desirabilit&aacute; de un nov lingua franca: on refusa continuar payar custosi traductores. It solmen va esser necessi far uniform grammatica, pronunciation e plu sommun paroles.
+
+Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc., li tot Europa usa li sam vocabularium. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules. Omnicos directe al desirabilit&aacute; de un nov lingua franca: on refusa continuar payar custosi traductores. It solmen va esser necessi far uniform grammatica, pronunciation e plu sommun paroles."),
+('The Elves', 3, "Elen sila lumenn omentilmo, Nae saian luume'. Cormamin lindua ele lle. Saesa omentien lle, vanya sulie Namaarie. Aa' lasser en lle coia orn n' omenta gurtha. Aa' i'sul nora lanne'lle. Aa' menle nauva calen ar' ta hwesta e' ale'quenle. Cormamin niuve tenna' ta elea lle au'. Lissenen ar' maska'lalaith tenna' lye omentuva.
+
+Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.")
