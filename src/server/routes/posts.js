37eb42d7162c7ba535ea3d0ef41394466c3e2778
Stephani McGrath <silentsoundz1977@gmail.com>
2017-12-12 14:04:13 -0800

    gets show page rendering correct info

--- src/server/routes/posts.js
+++ src/server/routes/posts.js
@@ -1,21 +1,35 @@
+const members = require('../../models/database/members')
 const posts = require('../../models/database/posts')
 const router = require('express').Router()
 
 
 router.get('/:postId', (request, response, next) => {
   const { postId } = request.params
+  const postAttributes = {}
+
   posts.getPostByPostId(postId)
     .then((post) => {
-      response.render('posts/post', {
-        title: post.title,
-        member_id: post.member_id,
-        post_entry: post.post_entry,
-      })
+      postAttributes.title = post.title
+      postAttributes.member_id = post.member_id
+      postAttributes.post_entry = post.post_entry
+    })
+    .then(() => {
+      members.getMemberById(postAttributes.member_id)
+        .then((member) => {
+          response.render('posts/post', {
+            title: postAttributes.title,
+            post_entry: postAttributes.post_entry,
+            iOwnThis: member.iOwnThis,
+            authenticated: member.authenticated,
+            full_name: member.full_name,
+            current_city: member.current_city,
+            join_date: member.date_joined,
+          })
+        })
     })
     .catch((error) => {
       next(error)
     })
 })
-
 
 module.exports = router
